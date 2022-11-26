3d72f1fb37c60d917a9ff6c74c783b6867ff4fe7
auroraptor <interestingbutno@yandex.ru>
2022-11-26 12:45:45 +0300

    feat: роут users

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
-// const { validateUserId, validateUser, validateAvatar } = require('../middlewares/validate');
-const {
-  updateUser, getCurrentUser,
-} = require('../controllers/users');
+const { validateUser } = require('../middlewares/validate');
+const { updateUser, getCurrentUser } = require('../controllers/users');
 
 router.get('/me', getCurrentUser);
 router.patch('/me', validateUser, updateUser);
