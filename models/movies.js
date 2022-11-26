ba74055693b678708c8021a6109b78567241c8c5
auroraptor <interestingbutno@yandex.ru>
2022-11-26 12:21:15 +0300

    feat: создать схему модель movie

---
+++ models/movies.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator(link) {
+        return validator.isURL(link) === true;
+      },
+      message: 'Is not a valid URL',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator(link) {
+        return validator.isURL(link) === true;
+      },
+      message: 'Is not a valid URL',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: String,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
