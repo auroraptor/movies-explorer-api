1622e4d680aea2f944b3cae96fb43a9371a1c6d1
auroraptor <interestingbutno@yandex.ru>
2022-11-28 15:33:00 +0300

    fix: rate limiter сконфигурирован в отдельном файле

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const limiter = require('./middlewares/limiter');
 const routes = require('./routes');
-// const cors = require('./middlewares/cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { errorHandler } = require('./middlewares/errorHandler');
 const { logNow, logError } = require('./utils/log');
@@ -19,25 +18,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(requestLogger);
 app.use(helmet());
-// app.use(cors);
+app.use(limiter);
 
-const { PORT = 3000, DB = 'mongodb://localhost:27017/moviesdb' } = process.env;
+const { PORT = 3000, DB = 'mongodb://localhost:27017/moviesdb-dev' } = process.env;
 
 mongoose.connect(DB, { autoIndex: true })
   .then(() => logNow('Connected to the server'))
   .catch((err) => logError(err));
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  standardHeaders: true,
-  legacyHeaders: false,
-});
-
-app.use(limiter);
-
 app.use(routes);
-
 app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
---
+++ middlewares/limiter.js
@@ -0,0 +1,8 @@
+const rateLimit = require('express-rate-limit');
+
+module.exports = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
